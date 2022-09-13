49646e95463d484b77e1b698d1e53cbc5399961c
Andrey582 <gandrey0606@mail.ru>
2022-09-13 20:50:41 +0300

    change

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import datetime
+import os
 
 if 20 > datetime.now().hour < 9:
     print("Еще не время")
     exit()
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-devshm-usage")
+chrome_options.add_argument("--no-sandbox")
+
 try:
-    driver = webdriver.Chrome("chromedriver.exe")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://lk.sut.ru/cabinet/")
     driver.find_element(By.ID, "users").send_keys("gnataly2106@mail.ru")
     driver.find_element(By.ID, "parole").send_keys("Andrey2002")
